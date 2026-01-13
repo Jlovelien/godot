17c7b1ea1b7237cedba62bba602d2fcde4ae16b2
Silverrida <oldschoola@gmail.com>
2026-01-12 20:13:01 -0500

    Okay this time for sure!

--- projects/praxprac/scripts/pillar.gd
+++ projects/praxprac/scripts/pillar.gd
@@ -1,1 +1,10 @@
-extends Area2D
+extends CharacterBody2D
+
+var gravity = 500
+
+func _physics_process(delta: float) -> void:
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	move_and_slide()
